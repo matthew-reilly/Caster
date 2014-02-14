03b086f26b0ceee0f013ce092b371f306ad6c290
whenyoubelieve2014 <whenyoubelieve2014@gmail.com>
2014-02-13 22:05:52 -0500

    add TV.html

--- server.js
+++ server.js
@@ -1,37 +1,36 @@
 // server.js
 
-	// modules =================================================
-	var express = require('express');
-	var app     = express();
-	var mongoose= require('mongoose');
+// modules =================================================
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
 
-	// configuration ===========================================
-		
-	// config files
-	var db = require('./config/db');
-    var Schema   = mongoose.Schema;
-	var port = process.env.PORT || 8080; // set our port
-	mongoose.connect(db.url); // connect to our mongoDB database (uncomment after you enter in your own credentials in config/db.js)
-	console.log(mongoose.connection.readyState);
+// configuration ===========================================
 
- 
+// config files
+var db = require('./config/db');
+var Schema = mongoose.Schema;
+var port = process.env.PORT || 8080; // set our port
+mongoose.connect(db.url); // connect to our mongoDB database (uncomment after you enter in your own credentials in config/db.js)
+console.log(mongoose.connection.readyState);
 
-	mongoose.connection.on('error', function (err) {
-	 // Do something
-	 console.log(err);
-	});
 
-	app.configure(function() {
-		app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
-		app.use(express.logger('dev')); 					// log every request to the console
-		app.use(express.bodyParser()); 						// have the ability to pull information from html in POST
-		app.use(express.methodOverride()); 					// have the ability to simulate DELETE and PUT
-	});
+mongoose.connection.on('error', function(err) {
+    // Do something
+    console.log(err);
+});
 
-	// routes ==================================================
-	require('./app/routes')(app); // configure our routes
+app.configure(function() {
+    app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+    app.use(express.logger('dev')); // log every request to the console
+    app.use(express.bodyParser()); // have the ability to pull information from html in POST
+    app.use(express.methodOverride()); // have the ability to simulate DELETE and PUT
+});
 
-	// start app ===============================================
-	app.listen(port);										// startup our app at http://localhost:8080
-	console.log('Magic happens on port ' + port); 			// shoutout to the user
-	exports = module.exports = app; 						// expose app
+// routes ==================================================
+require('./app/routes')(app); // configure our routes
+
+// start app ===============================================
+app.listen(port); // startup our app at http://localhost:8080
+console.log('Magic happens on port ' + port); // shoutout to the user
+exports = module.exports = app; // expose app
