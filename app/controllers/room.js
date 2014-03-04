813ae171b1c2cf698fd2190d1645deb8ce3fa670
Jared Pavan <thatsjared87@gmail.com>
2014-03-03 20:42:09 -0500

    Fixing indents

--- app/controllers/room.js
+++ app/controllers/room.js
@@ -143,18 +143,18 @@ exports.dealCards = function(req, res) {
 
 //Shuffle list. This is the most efficient way to return a randomized list, as seen here http://bost.ocks.org/mike/shuffle/
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-  return array;
+	var currentIndex = array.length, temporaryValue, randomIndex;
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+	array[randomIndex] = temporaryValue;
+	}
+	return array;
 }
 
 //Function countPlayerCards
